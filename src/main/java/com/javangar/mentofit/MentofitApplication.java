3b5ba02ca52048fb3e8b37232ca79e05c0fc7f04
Javanshir Mammadov <j4v4n5h1r@gmail.com>
2023-05-31 17:10:52 +0400

    x

---
+++ src/main/java/com/javangar/mentofit/MentofitApplication.java
@@ -0,0 +1,13 @@
+package com.javangar.mentofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentofitApplication.class, args);
+	}
+
+}
